2cd0c1f16d7f84d3be8d066ef4e89eaa325ba626
Zeniittt <tinnqd1702@gmail.com>
2023-06-13 07:15:53 +0700

    4.6. Collision Check

--- My Warrior/Assets/Player.cs
+++ My Warrior/Assets/Player.cs
@@ -8,6 +8,13 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce;
 
+    [Header("Collision Information")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private float wallCheckDistance;
+
     #region Components
 
     public Animator anim { get; private set; }
@@ -50,6 +57,14 @@ public class Player : MonoBehaviour
     public void SetVelocity(float _xVelocity, float _yVelocity)
     {
         rb.velocity = new Vector2(_xVelocity, _yVelocity);
+    }
+
+    public bool IsGroundedDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
     }
 
 }
