b0113cc75f4ce1cec8eb18dc60d08540d2e39ea2
Zeniittt <tinnqd1702@gmail.com>
2023-06-14 16:30:56 +0700

    6.6. Finalize Battle State (Done Section 6)

--- My Warrior/Assets/SkeletonBattleState.cs
+++ My Warrior/Assets/SkeletonBattleState.cs
@@ -26,11 +26,16 @@ public class SkeletonBattleState : EnemyState
 
         if(enemy.IsPlayerDetected())
         {
+            stateTimer = enemy.battleTime;
             if(enemy.IsPlayerDetected().distance < enemy.attackDistance)
             {
                 if(CanAttack())
                     stateMachine.ChangeState(enemy.attackState);
             }
+        }else
+        {
+            if (stateTimer < 0 || Vector2.Distance(player.transform.position, enemy.transform.position) > 7)
+                stateMachine.ChangeState(enemy.idleState);  
         }
 
         if (player.position.x > enemy.transform.position.x)
