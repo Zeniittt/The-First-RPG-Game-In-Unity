d9eb74d101ee0bdb9ec425360289ad644f2de75f
Zeniittt <tinnqd1702@gmail.com>
2023-06-14 12:47:05 +0700

    5.2. Making inheritance for Player and Enemy

--- My Warrior/Assets/Scipts/Player/Player.cs
+++ My Warrior/Assets/Scipts/Player/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Entity
 {
     public bool isBusy { get; private set; }
 
@@ -20,23 +20,6 @@ public class Player : MonoBehaviour
     public float dashDuration;
     public float dashDirection { get; private set; }
 
-    [Header("Collision Information")]
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundCheckDistance;
-    [SerializeField] private LayerMask whatIsGround;
-    [SerializeField] private Transform wallCheck;
-    [SerializeField] private float wallCheckDistance;
-
-    //[Header()]
-    public int facingDirection { get; private set; } = 1;
-    private bool facingRight = true;
-
-    #region Components
-
-    public Animator anim { get; private set; }
-    public Rigidbody2D rb { get; private set; }
-
-    #endregion
     #region States
 
     public PlayerStateMachine stateMachine { get; private set; }
@@ -51,8 +34,10 @@ public class Player : MonoBehaviour
     public PlayerPrimaryAttackState primaryAttack { get; private set; }
     #endregion
 
-    private void Awake()
+    protected override void Awake()
     {
+        base.Awake();
+
         stateMachine = new PlayerStateMachine();
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
         moveState = new PlayerMoveState(this, stateMachine, "Move");
@@ -64,30 +49,20 @@ public class Player : MonoBehaviour
         primaryAttack = new PlayerPrimaryAttackState(this, stateMachine, "Attack");
     }
 
-    private void Start()
+    protected override void Start()
     {
-        anim = GetComponentInChildren<Animator>();
-        rb = GetComponent<Rigidbody2D>();
+        base.Start();
+
         stateMachine.Initialize(idleState);
     }
 
-    private void Update()
+    protected override void Update()
     {
+        base.Update();
+
         stateMachine.currentState.Update();
         CheckForDashInput();
     }
-
-    #region Velocity
-
-    public void ZeroVelocity() => rb.velocity = new Vector2(0, 0);
-
-    public void SetVelocity(float _xVelocity, float _yVelocity)
-    {
-        rb.velocity = new Vector2(_xVelocity, _yVelocity);
-        FlipController(_xVelocity);
-    }
-
-    #endregion
 
     public IEnumerator BusyFor(float _seconds)
     {
@@ -95,37 +70,6 @@ public class Player : MonoBehaviour
         yield return new WaitForSeconds(_seconds);
         isBusy = false;
     }
-
-    #region Collision
-
-    public bool IsGroundedDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
-    public bool IsWallDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDirection, wallCheckDistance, whatIsGround);
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
-        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
-    }
-
-    #endregion
-
-    #region Flip
-
-    public void Flip()
-    {
-        facingDirection = facingDirection * -1;
-        facingRight = !facingRight;
-        transform.Rotate(0, 180, 0);
-    }
-
-    public void FlipController(float _x)
-    {
-        if (_x > 0 && !facingRight) Flip();
-        else 
-            if(_x < 0 && facingRight) Flip();
-    }
-
-    #endregion
 
     public void CheckForDashInput()
     {
