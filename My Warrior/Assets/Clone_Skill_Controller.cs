178a716cb0a605c3becbb85cb9e9bfebb09b33b5
Zeniittt <tinnqd1702@gmail.com>
2023-06-16 16:34:58 +0700

    9.2. Clone's Attack (Done Section 9)

--- My Warrior/Assets/Clone_Skill_Controller.cs
+++ My Warrior/Assets/Clone_Skill_Controller.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class Clone_Skill_Controller : MonoBehaviour
 {
     private SpriteRenderer sr;
+    private Animator anim;
     [SerializeField] private float colorLoosingSpeed;
 
     private float cloneTimer;
+    [SerializeField] private Transform attackCheck;
+    [SerializeField] private float attackCheckRadius = .8f;
+    private Transform closestEnemy;
 
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -20,12 +26,62 @@ public class Clone_Skill_Controller : MonoBehaviour
         if(cloneTimer < 0)
         {
             sr.color = new Color(1, 1, 1, sr.color.a - (Time.deltaTime * colorLoosingSpeed));
+            if (sr.color.a <= 0)
+                Destroy(gameObject);
+        }
+
+    }
+
+    public void SetupClone(Transform _newTransform, float _cloneDuration, bool _canAttack)
+    {
+        if (_canAttack)
+            anim.SetInteger("AttackNumber", Random.Range(1, 3));
+
+        transform.position = _newTransform.position;
+        cloneTimer = _cloneDuration;
+
+        FaceClosestTarget();
+    }
+
+    private void AnimationTrigger()
+    {
+        cloneTimer = -.1f;
+    }
+
+    private void AttackTrigger()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(attackCheck.position, attackCheckRadius);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+                hit.GetComponent<Enemy>().Damage();
         }
     }
 
-    public void SetupClone(Transform _newTransform, float _cloneDuration)
+    private void FaceClosestTarget()
     {
-        transform.position = _newTransform.position;
-        cloneTimer = _cloneDuration;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 25);
+
+        float closestDistance = Mathf.Infinity;
+
+        foreach(var hit in colliders)
+        {
+            if(hit.GetComponent<Enemy>() != null)
+            {
+                float distanceToEnemy = Vector2.Distance(transform.position, hit.transform.position);
+                if (distanceToEnemy < closestDistance)
+                {
+                    closestDistance = distanceToEnemy;
+                    closestEnemy = hit.transform;
+                }
+            }
+        }
+
+        if(closestEnemy != null)
+        {
+            if (transform.position.x > closestEnemy.position.x)
+                transform.Rotate(0, 180, 0);
+        }
     }
 }
--- My Warrior/Assets/Scipts/Skills/Clone_Skill.cs
+++ My Warrior/Assets/Scipts/Skills/Clone_Skill.cs
@@ -7,10 +7,12 @@ public class Clone_Skill : Skill
     [Header("Clone Information")]
     [SerializeField] private GameObject clonePrefabs;
     [SerializeField] private float cloneDuration;
+    [Space]
+    [SerializeField] private bool canAttack;
 
     public void CreateClone(Transform _clonePosition)
     {
         GameObject newClone = Instantiate(clonePrefabs);
-        newClone.GetComponent<Clone_Skill_Controller>().SetupClone(_clonePosition, cloneDuration);
+        newClone.GetComponent<Clone_Skill_Controller>().SetupClone(_clonePosition, cloneDuration, canAttack);
     }
 }
