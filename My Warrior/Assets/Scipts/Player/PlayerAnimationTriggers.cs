e0289c59e702fc6ffa6d8cd521391baee93cc0a6
Zeniittt <tinnqd1702@gmail.com>
2023-06-17 16:08:29 +0700

    10.2. Setting up details of the sword (have bug when throw sword)

--- My Warrior/Assets/Scipts/Player/PlayerAnimationTriggers.cs
+++ My Warrior/Assets/Scipts/Player/PlayerAnimationTriggers.cs
@@ -20,4 +20,9 @@ public class PlayerAnimationTriggers : MonoBehaviour
                 hit.GetComponent<Enemy>().Damage();
         }
     }
+
+    private void ThrowSword()
+    {
+        SkillManager.instance.sword.CreateSword();
+    }
 }
