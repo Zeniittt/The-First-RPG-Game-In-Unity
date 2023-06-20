ca2b661c9ffe225dfb1e549ce354f0af219d3a2a
Zeniittt <tinnqd1702@gmail.com>
2023-06-20 21:23:45 +0700

    12.2. Explosive crystal (Exist bug animation, don't worry bro)

--- My Warrior/Assets/Crystal_Skill.cs
+++ My Warrior/Assets/Crystal_Skill.cs
@@ -8,6 +8,13 @@ public class Crystal_Skill : Skill
     [SerializeField] private GameObject crystalPrefab;
     private GameObject currentCrystal;
 
+    [Header("Explosive crystal")]
+    [SerializeField] private bool canExplode;
+
+    [Header("Moving Crystal")]
+    [SerializeField] private bool canMoveToEnemy;
+    [SerializeField] private float moveSpeed;
+
     public override void UseSkill()
     {
         base.UseSkill();
@@ -17,12 +24,15 @@ public class Crystal_Skill : Skill
             currentCrystal = Instantiate(crystalPrefab, player.transform.position, Quaternion.identity);
             Crystal_Skill_Controller currentCrystalScript = currentCrystal.GetComponent<Crystal_Skill_Controller>();
 
-            currentCrystalScript.SetupCrystal(crystalDuration);
+            currentCrystalScript.SetupCrystal(crystalDuration, canExplode, canMoveToEnemy, moveSpeed);
         }
         else
         {
+            Vector2 playerPosition = player.transform.position;
+
             player.transform.position = currentCrystal.transform.position;
-            Destroy(currentCrystal);
+            currentCrystal.transform.position = playerPosition;
+            currentCrystal.GetComponent<Crystal_Skill_Controller>()?.FinishCrystal();
         }
     }
 }
