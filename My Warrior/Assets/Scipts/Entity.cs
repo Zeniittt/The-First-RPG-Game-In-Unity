d75054bef3792e8dec31a681140bf1711b065f3a
Zeniittt <tinnqd1702@gmail.com>
2024-07-02 21:12:58 +0700

    13.1 Character stats

--- My Warrior/Assets/Scipts/Entity.cs
+++ My Warrior/Assets/Scipts/Entity.cs
@@ -27,6 +27,7 @@ public class Entity : MonoBehaviour
     public Rigidbody2D rb { get; private set; }
     public EntityFX fx { get; private set; }
     public SpriteRenderer sr { get; private set; }
+    public CharacterStats stats { get; private set; }
 
     #endregion
 
@@ -41,6 +42,7 @@ public class Entity : MonoBehaviour
         anim = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody2D>();
         fx = GetComponent<EntityFX>();
+        stats = GetComponent<CharacterStats>();
     }
 
     protected virtual void Update()
