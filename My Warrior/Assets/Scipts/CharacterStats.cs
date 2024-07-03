d22d1d0fbaaed9d0464b6d544046a2dfa8f2f946
Zeniittt <tinnqd1702@gmail.com>
2024-07-03 11:55:40 +0700

    13.3 Applying stats to combat

--- My Warrior/Assets/Scipts/CharacterStats.cs
+++ My Warrior/Assets/Scipts/CharacterStats.cs
@@ -1,24 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    public Stat strength;
     public Stat damage;
     public Stat maxHealth;
 
     [SerializeField] private int currentHealth;
 
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
         currentHealth = maxHealth.GetValue();
-
-        damage.AddModifier(4);
     }
 
-    public void TakeDamage(int _damage)
+    public virtual void DoDamage(CharacterStats _targetStats)
+    {
+        int totalDamage = damage.GetValue() + strength.GetValue();
+
+        _targetStats.TakeDamage(totalDamage);
+    }
+
+    public virtual void TakeDamage(int _damage)
     {
         currentHealth -= _damage;
+
+        Debug.Log(_damage);
+
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    protected virtual void Die()
+    {
+        
     }
 }
