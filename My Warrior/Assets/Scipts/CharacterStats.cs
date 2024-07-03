1529d77f86a9e6869a96db6bfad5dcc5e9b4bc95
Zeniittt <tinnqd1702@gmail.com>
2024-07-03 11:09:00 +0700

    13.2 Stat script

--- My Warrior/Assets/Scipts/CharacterStats.cs
+++ My Warrior/Assets/Scipts/CharacterStats.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public int damage;
-    public int maxHealth;
+    public Stat damage;
+    public Stat maxHealth;
 
     [SerializeField] private int currentHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth = maxHealth;
+        currentHealth = maxHealth.GetValue();
+
+        damage.AddModifier(4);
     }
 
     public void TakeDamage(int _damage)
