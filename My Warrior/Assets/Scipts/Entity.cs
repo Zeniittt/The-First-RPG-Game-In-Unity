22f80870e2b6f1bde4608e8c66574f874731c028
Zeniittt <tinnqd1702@gmail.com>
2024-07-04 15:28:35 +0700

    recommit

--- My Warrior/Assets/Scipts/Entity.cs
+++ My Warrior/Assets/Scipts/Entity.cs
@@ -129,13 +129,6 @@ public class Entity : MonoBehaviour
 
     #endregion
 
-    public void MakeTransparent(bool _transparent)
-    {
-        if (_transparent)
-            sr.color = Color.clear;
-        else
-            sr.color = Color.white;
-    }
 
     public virtual void Die()
     {
