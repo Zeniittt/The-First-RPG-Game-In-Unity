bd00cc2c36d431c627bbc1ee2427a304390b2cc1
Zeniittt <tinnqd1702@gmail.com>
2024-07-05 17:19:33 +0700

    14.7. Apply item modifiers

--- My Warrior/Assets/Scipts/Items and Inventory/ItemData_Equipment.cs
+++ My Warrior/Assets/Scipts/Items and Inventory/ItemData_Equipment.cs
@@ -12,4 +12,72 @@ public enum EquipmentType
 public class ItemData_Equipment : ItemData
 {
     public EquipmentType equipmentType;
+
+    [Header("Major Stats")]
+    public int strength;       // 1 point increase damage by 1 and crit power 1%
+    public int agility;        // 1 point increase evasion by 1% and crit chance by 1%
+    public int intelligence;   // 1 point increase magic damage by 1 and magic resistance by 3
+    public int vitality;       // 1 point increase health by 3 or 5
+
+    [Header("Offensive Stats")]
+    public int damage;
+    public int critChance;
+    public int critPower;
+
+    [Header("Defensive Stats")]
+    public int maxHealth;
+    public int armor;
+    public int evasion;
+    public int magicResistance;
+
+    [Header("Magic Stats")]
+    public int fireDamage;
+    public int iceDamage;
+    public int lightingDamage;
+
+    public void AddModifiers()
+    {
+        PlayerStats playerStats = PlayerManager.instance.player.GetComponent<PlayerStats>();
+
+        playerStats.strength.AddModifier(strength);
+        playerStats.agility.AddModifier(agility);
+        playerStats.intelligence.AddModifier(intelligence);
+        playerStats.vitality.AddModifier(vitality);
+
+        playerStats.damage.AddModifier(damage);
+        playerStats.critChance.AddModifier(critChance);
+        playerStats.critPower.AddModifier(critPower);
+
+        playerStats.maxHealth.AddModifier(maxHealth);
+        playerStats.armor.AddModifier(armor);
+        playerStats.evasion.AddModifier(evasion);
+        playerStats.magicResistance.AddModifier(magicResistance);
+
+        playerStats.fireDamage.AddModifier(fireDamage);
+        playerStats.iceDamage.AddModifier(iceDamage);
+        playerStats.lightingDamage.AddModifier(lightingDamage);
+    }
+
+    public void RemoveModifiers()
+    {
+        PlayerStats playerStats = PlayerManager.instance.player.GetComponent<PlayerStats>();
+
+        playerStats.strength.RemoveModifier(strength);
+        playerStats.agility.RemoveModifier(agility);
+        playerStats.intelligence.RemoveModifier(intelligence);
+        playerStats.vitality.RemoveModifier(vitality);
+
+        playerStats.damage.RemoveModifier(damage);
+        playerStats.critChance.RemoveModifier(critChance);
+        playerStats.critPower.RemoveModifier(critPower);
+
+        playerStats.maxHealth.RemoveModifier(maxHealth);
+        playerStats.armor.RemoveModifier(armor);
+        playerStats.evasion.RemoveModifier(evasion);
+        playerStats.magicResistance.RemoveModifier(magicResistance);
+
+        playerStats.fireDamage.RemoveModifier(fireDamage);
+        playerStats.iceDamage.RemoveModifier(iceDamage);
+        playerStats.lightingDamage.RemoveModifier(lightingDamage);
+    }
 }
