9c222820bb2c289d2378f42349977f4dc34f939c
Zeniittt <tinnqd1702@gmail.com>
2024-07-05 17:36:29 +0700

    14.8. Enemy modifiers

--- My Warrior/Assets/Scipts/Stats/EnemyStats.cs
+++ My Warrior/Assets/Scipts/Stats/EnemyStats.cs
@@ -6,10 +6,50 @@ public class EnemyStats : CharacterStats
 {
     Enemy enemy;
 
+    [Header("Level detail")]
+    [SerializeField] private int level = 1;
+
+    [Range(0f, 1f)]
+    [SerializeField] private float percentageModifier = .4f;
+
     protected override void Start()
     {
+        ApplyLevelModifiers();
+
         base.Start();
         enemy = GetComponent<Enemy>();
+
+    }
+
+    private void ApplyLevelModifiers()
+    {
+        Modify(strength);
+        Modify(agility);
+        Modify(intelligence);
+        Modify(vitality);
+
+        Modify(damage);
+        Modify(critChance);
+        Modify(critPower);
+
+        Modify(maxHealth);
+        Modify(armor);
+        Modify(evasion);
+        Modify(magicResistance);
+
+        Modify(fireDamage);
+        Modify(iceDamage);
+        Modify(lightingDamage);
+    }
+
+    private void Modify(Stat _stat)
+    {
+        for (int i = 0; i < level; i++)
+        {
+            float modifier = _stat.GetValue() * percentageModifier;
+
+            _stat.AddModifier(Mathf.RoundToInt(modifier));
+        }
     }
 
     public override void TakeDamage(int _damage)
