5f43984584398f8c8f29ed39757e47b9df4a81ae
Zeniittt <tinnqd1702@gmail.com>
2024-07-08 15:19:42 +0700

    15.5. Stat Tooltip

--- My Warrior/Assets/Scipts/UI/UI_StatSlot.cs
+++ My Warrior/Assets/Scipts/UI/UI_StatSlot.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class UI_StatSlot : MonoBehaviour
+public class UI_StatSlot : MonoBehaviour,  IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] private string statName;
+    private UI ui;
 
+    [SerializeField] private string statName;
     [SerializeField] private StatType statType;
     [SerializeField] private TextMeshProUGUI statValueText;
     [SerializeField] private TextMeshProUGUI statNameText;
 
+    [TextArea]
+    [SerializeField] private string statDescription;
 
     private void OnValidate()
     {
@@ -23,6 +27,8 @@ public class UI_StatSlot : MonoBehaviour
     void Start()
     {
         UpdateStatValueUI();
+
+        ui = GetComponentInParent<UI>();
     }
 
     public void UpdateStatValueUI()
@@ -32,6 +38,35 @@ public class UI_StatSlot : MonoBehaviour
         if(playerStats != null)
         {
             statValueText.text = playerStats.GetStat(statType).GetValue().ToString();
+
+
+            if (statType == StatType.maxHealth)
+                statValueText.text = playerStats.GetMaxHealthValue().ToString();
+
+            if (statType == StatType.damage)
+                statValueText.text = (playerStats.damage.GetValue() + playerStats.strength.GetValue()).ToString();
+
+            if (statType == StatType.critPower)
+                statValueText.text = (playerStats.critPower.GetValue() + playerStats.strength.GetValue()).ToString();
+
+            if (statType == StatType.critChance)
+                statValueText.text = (playerStats.critChance.GetValue() + playerStats.agility.GetValue()).ToString();
+
+            if (statType == StatType.evasion)
+                statValueText.text = (playerStats.evasion.GetValue() + playerStats.agility.GetValue()).ToString();
+
+            if (statType == StatType.magicResistance)
+                statValueText.text = (playerStats.magicResistance.GetValue() + (playerStats.intelligence.GetValue() * 3)).ToString();
         }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ui.statToolTip.ShowStatToolTip(statDescription);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ui.statToolTip.HideStatToolTip();
     }
 }
