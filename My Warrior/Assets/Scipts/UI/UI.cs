ad354e88f9eeca488de9e2cad5eb573dea813878
Zeniittt <tinnqd1702@gmail.com>
2024-07-08 15:48:02 +0700

    15.6. Switch UI with Keyboard

--- My Warrior/Assets/Scipts/UI/UI.cs
+++ My Warrior/Assets/Scipts/UI/UI.cs
@@ -1,21 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UI : MonoBehaviour
 {
+    [SerializeField] private GameObject characterUI;
+    [SerializeField] private GameObject skillTreeUI;
+    [SerializeField] private GameObject craftUI;
+    [SerializeField] private GameObject optionsUI;
+
     public UI_ItemToolTip itemToolTip;
     public UI_StatToolTip statToolTip;
 
     void Start()
     {
+        SwitchTo(null);
 
+        itemToolTip.gameObject.SetActive(false);
+        statToolTip.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Tab))
+            SwitchWithKeyTo(characterUI);
+
+        if (Input.GetKeyUp(KeyCode.K))
+            SwitchWithKeyTo(skillTreeUI);
+
+        if (Input.GetKeyUp(KeyCode.P))
+            SwitchWithKeyTo(craftUI);
+
+        if (Input.GetKeyUp(KeyCode.O))
+            SwitchWithKeyTo(optionsUI);
     }
 
     public void SwitchTo(GameObject _menu)
@@ -27,5 +43,16 @@ public class UI : MonoBehaviour
 
         if (_menu != null)
             _menu.SetActive(true);
+    }
+
+    public void SwitchWithKeyTo(GameObject _menu)
+    {
+        if (_menu != null && _menu.activeSelf)
+        {
+            _menu.SetActive(false);
+            return;
+        }
+
+        SwitchTo(_menu);
     }
 }
