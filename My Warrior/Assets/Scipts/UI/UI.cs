1af779dc74cd567fdde3e445ff22419fd5d1938b
Zeniittt <tinnqd1702@gmail.com>
2024-07-10 21:25:03 +0700

    15.16. Skill Tree Hot Fix

--- My Warrior/Assets/Scipts/UI/UI.cs
+++ My Warrior/Assets/Scipts/UI/UI.cs
@@ -12,6 +12,11 @@ public class UI : MonoBehaviour
     public UI_CraftWindow craftWindow;
     public UI_SkillToolTip skillToolTip;
 
+    private void Awake()
+    {
+        SwitchTo(skillTreeUI);
+    }
+
     void Start()
     {
         SwitchTo(null);
