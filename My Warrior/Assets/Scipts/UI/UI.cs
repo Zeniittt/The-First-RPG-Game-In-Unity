579fce2f62acd708dc501a929369c235f38fc0a2
Zeniittt <tinnqd1702@gmail.com>
2024-07-13 18:01:44 +0700

    fix bug lost Ingame_UI

--- My Warrior/Assets/Scipts/UI/UI.cs
+++ My Warrior/Assets/Scipts/UI/UI.cs
@@ -86,7 +86,7 @@ public class UI : MonoBehaviour, ISaveManager
     {
         for (int i = 0; i < transform.childCount; i++)
         {
-            if (transform.GetChild(i).gameObject.activeSelf)
+            if (transform.GetChild(i).gameObject.activeSelf && transform.GetChild(i).GetComponent<UI_FadeScreen>() == null)
                 return;
         }
 
