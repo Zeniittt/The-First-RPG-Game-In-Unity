eeeed0aa97a5b8f0cd7cef74582359eb166cf615
Zeniittt <tinnqd1702@gmail.com>
2024-09-25 09:41:58 +0700

    18.6.Area sound

--- My Warrior/Assets/Scipts/Managers/AudioManager.cs
+++ My Warrior/Assets/Scipts/Managers/AudioManager.cs
@@ -58,6 +58,26 @@ public class AudioManager : MonoBehaviour
 
     public void StopSFX(int _sfxIndex) => sfx[_sfxIndex].Stop();
 
+    public void StopSFXWithTime(int _index) => StartCoroutine(DecreaseVolume(sfx[_index]));
+
+    private IEnumerator DecreaseVolume(AudioSource _audio)
+    {
+        float defaultVolume = _audio.volume;
+
+        while(_audio.volume > .1f)
+        {
+            _audio.volume -= _audio.volume * .2f;
+            yield return new WaitForSeconds(.6f);
+
+            if(_audio.volume <= .1f)
+            {
+                _audio.Stop();
+                _audio.volume = defaultVolume;
+                break;
+            }
+        }
+    }
+
     public void PlayRandomBGM()
     {
         bgmIndex = Random.Range(0, bgm.Length);
