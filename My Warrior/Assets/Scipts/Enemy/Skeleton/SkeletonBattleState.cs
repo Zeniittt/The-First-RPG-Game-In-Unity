ca3c3252dd2d9389fd8b7e6f4e9feab8549c3d80
Zeniittt <tinnqd1702@gmail.com>
2024-09-26 13:08:14 +0700

    19.3.Improving skeleton

--- My Warrior/Assets/Scipts/Enemy/Skeleton/SkeletonBattleState.cs
+++ My Warrior/Assets/Scipts/Enemy/Skeleton/SkeletonBattleState.cs
@@ -18,6 +18,9 @@ public class SkeletonBattleState : EnemyState
         base.Enter();
 
         player = PlayerManager.instance.player.transform;
+
+        if (player.GetComponent<PlayerStats>().isDead)
+            stateMachine.ChangeState(enemy.moveState);
     }
 
     public override void Update()
@@ -56,6 +59,7 @@ public class SkeletonBattleState : EnemyState
     {
         if(Time.time >= enemy.lastTimeAttacked + enemy.attackCooldown)
         {
+            enemy.attackCooldown = Random.Range(enemy.minAttackCooldown, enemy.maxAttackCooldown);
             enemy.lastTimeAttacked = Time.time;
             return true;
         }
