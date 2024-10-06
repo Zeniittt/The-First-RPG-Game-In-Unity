2e40258f6ece9c369ab675a933602fd9011c95d4
Zeniittt <tinnqd1702@gmail.com>
2024-10-06 15:19:16 +0700

    Slime - stunned state improvement

--- My Warrior/Assets/Scipts/Enemy/Slime/SlimeStunnedState.cs
+++ My Warrior/Assets/Scipts/Enemy/Slime/SlimeStunnedState.cs
@@ -24,12 +24,20 @@ public class SlimeStunnedState : EnemyState
     {
         base.Exit();
 
-        enemy.fx.Invoke("CancelColorChange", 0);
+        enemy.stats.MakeInvincible(false);
+
     }
 
     public override void Update()
     {
         base.Update();
+
+        if (rb.velocity.y < .1f && enemy.IsGroundedDetected())
+        {
+            enemy.fx.Invoke("CancelColorChange", 0);
+            enemy.anim.SetTrigger("StunFold");
+            enemy.stats.MakeInvincible(true);
+        }
 
         if (stateTimer < 0)
             stateMachine.ChangeState(enemy.idleState);
--- My Warrior/Assets/Scipts/Stats/CharacterStats.cs
+++ My Warrior/Assets/Scipts/Stats/CharacterStats.cs
@@ -133,6 +133,9 @@ public class CharacterStats : MonoBehaviour
     {
         bool criticalStrike = false;
 
+        if (_targetStats.isInvincible)
+            return;
+
         if (TargetCanAvoidAttack(_targetStats))
             return;
 
